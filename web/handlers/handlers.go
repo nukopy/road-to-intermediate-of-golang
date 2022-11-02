66d6d455a00dd29b1e0a84e36ff55499d8beb321
nukopy <pytwbf201830@gmail.com>
2022-11-03 00:28:57 +0900

    refactor

--- web/handlers/handlers.go
+++ web/handlers/handlers.go
@@ -29,29 +29,23 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func GetArticlesHandler(w http.ResponseWriter, req *http.Request) {
-	// extract query parameter "page"
 	qp := req.URL.Query()
-	log.Printf("Query Params: %v\n", qp)
-	pageValues, ok := qp["page"]
+	// log.Printf("Query Params: %v\n", qp)
 
-	// page パラメータがないとき、page=1 としてレスポンスを返す
-	if !ok {
-		page := 1
-		n, err := io.WriteString(w, fmt.Sprintf("Articles List - Page %d\n", page))
+	var page int
+	if pageValues, ok := qp["page"]; ok {
+		// page パラメータが 1 回以上指定されている場合、最初の値を使用する
+		pageStr := pageValues[0]
+		var err error
+		page, err = strconv.Atoi(pageStr)
 		if err != nil {
-			log.Fatal(err)
+			// page パラメータが整数に変換できない場合、400 エラーレスポンスを返す
+			http.Error(w, fmt.Sprintf("Invalid query parameter: \"%s\"\n", pageStr), http.StatusBadRequest)
+			return
 		}
-		log.Printf("getArticleListHandler: %d bytes were written.\n", n)
-		return
-	}
-
-	// page パラメータが 1 回以上指定されている場合、最初の値を指定する
-	pageStr := pageValues[0]
-	page, err := strconv.Atoi(pageStr)
-	if err != nil {
-		// page パラメータが整数に変換できない場合、400 エラーレスポンスを返す
-		http.Error(w, fmt.Sprintf("Invalid query parameter: \"%s\"\n", pageStr), http.StatusBadRequest)
-		return
+	} else {
+		// page パラメータが指定されなかった場合、page = 1 として扱う
+		page = 1
 	}
 
 	// 指定された page パラメータに応じてレスポンスを返す
