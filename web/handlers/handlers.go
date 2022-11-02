8147836cdd8afbf885831337e25651768079d1e0
nukopy <pytwbf201830@gmail.com>
2022-11-03 00:20:02 +0900

    feat: impl handling query parameter

--- web/handlers/handlers.go
+++ web/handlers/handlers.go
@@ -29,7 +29,33 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func GetArticlesHandler(w http.ResponseWriter, req *http.Request) {
-	n, err := io.WriteString(w, "Article List\n")
+	// extract query parameter "page"
+	qp := req.URL.Query()
+	log.Printf("Query Params: %v\n", qp)
+	pageValues, ok := qp["page"]
+
+	// page パラメータがないとき、page=1 としてレスポンスを返す
+	if !ok {
+		page := 1
+		n, err := io.WriteString(w, fmt.Sprintf("Articles List - Page %d\n", page))
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("getArticleListHandler: %d bytes were written.\n", n)
+		return
+	}
+
+	// page パラメータが 1 回以上指定されている場合、最初の値を指定する
+	pageStr := pageValues[0]
+	page, err := strconv.Atoi(pageStr)
+	if err != nil {
+		// page パラメータが整数に変換できない場合、400 エラーレスポンスを返す
+		http.Error(w, fmt.Sprintf("Invalid query parameter: \"%s\"\n", pageStr), http.StatusBadRequest)
+		return
+	}
+
+	// 指定された page パラメータに応じてレスポンスを返す
+	n, err := io.WriteString(w, fmt.Sprintf("Articles List - Page %d\n", page))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,6 +65,7 @@ func GetArticlesHandler(w http.ResponseWriter, req *http.Request) {
 func GetArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	articleId, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
+		// ルーティングの実装上ここには来ないはず
 		http.Error(w, "Invalid path parameter", http.StatusBadRequest)
 		return
 	}
